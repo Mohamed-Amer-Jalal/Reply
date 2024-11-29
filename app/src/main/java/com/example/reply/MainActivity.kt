2916da7f525a12de299353b6988bb39a036b5225
M-Amer <mohamedamerjalal@gmail.com>
2024-11-29 18:05:29 +0200

    Previews for different screen sizes

--- app/src/main/java/com/example/reply/MainActivity.kt
+++ app/src/main/java/com/example/reply/MainActivity.kt
@@ -12,9 +12,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.reply.ui.ReplyApp
 import com.example.reply.ui.theme.ReplyTheme
 
@@ -40,6 +43,75 @@ class MainActivity : ComponentActivity() {
                         windowSize = windowSize.widthSizeClass
                     )
                 }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ReplyAppCompactPreview() {
+    ReplyTheme {
+        ReplyTheme {
+            val layoutDirection = LocalLayoutDirection.current
+            val windowInsets = WindowInsets.safeDrawing.asPaddingValues()
+            Scaffold(
+                modifier = Modifier
+                    .padding(
+                        start = windowInsets.calculateStartPadding(layoutDirection),
+                        end = windowInsets.calculateEndPadding(layoutDirection)
+                    )
+            ) { innerPadding ->
+                ReplyApp(
+                    modifier = Modifier.padding(innerPadding),
+                    windowSize = WindowWidthSizeClass.Compact
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 700)
+@Composable
+fun ReplyAppMediumPreview() {
+    ReplyTheme {
+        ReplyTheme {
+            val layoutDirection = LocalLayoutDirection.current
+            val windowInsets = WindowInsets.safeDrawing.asPaddingValues()
+            Scaffold(
+                modifier = Modifier
+                    .padding(
+                        start = windowInsets.calculateStartPadding(layoutDirection),
+                        end = windowInsets.calculateEndPadding(layoutDirection)
+                    )
+            ) { innerPadding ->
+                ReplyApp(
+                    modifier = Modifier.padding(innerPadding),
+                    windowSize = WindowWidthSizeClass.Medium
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 1000)
+@Composable
+fun ReplyAppExpandedPreview() {
+    ReplyTheme {
+        ReplyTheme {
+            val layoutDirection = LocalLayoutDirection.current
+            val windowInsets = WindowInsets.safeDrawing.asPaddingValues()
+            Scaffold(
+                modifier = Modifier
+                    .padding(
+                        start = windowInsets.calculateStartPadding(layoutDirection),
+                        end = windowInsets.calculateEndPadding(layoutDirection)
+                    )
+            ) { innerPadding ->
+                ReplyApp(
+                    modifier = Modifier.padding(innerPadding),
+                    windowSize = WindowWidthSizeClass.Expanded
+                )
             }
         }
     }
